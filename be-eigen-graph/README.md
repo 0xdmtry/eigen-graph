1792d59d137c2711012aaeb615b6d69d27df0759
0xdmtry <0xdmtry@gmail.com>
2025-09-25 11:01:07 -0400

    backend: init the subgraph model, re-alignment of the code, adding an additional command for readme

--- be-eigen-graph/README.md
+++ be-eigen-graph/README.md
@@ -11,3 +11,9 @@ cargo fmt -- --check
 ```bash 
 cargo clippy --all-targets --all-features -- -D warnings
 ```
+
+Allows unused code
+
+```bash
+cargo clippy --all-targets --all-features -- -D warnings -A unused -A dead_code
+```
---
+++ be-eigen-graph/src/models/subgraph.rs
@@ -0,0 +1,31 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Serialize)]
+pub struct GraphQLRequest<'q, V> {
+    pub query: &'q str,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub variables: Option<V>,
+}
+
+#[derive(Deserialize)]
+pub struct GraphQLResponse<T> {
+    pub data: Option<T>,
+    #[serde(default)]
+    pub errors: Option<Vec<GraphQLError>>,
+}
+
+#[derive(Deserialize)]
+pub struct GraphQLError {
+    pub message: String,
+    #[serde(default)]
+    pub path: Option<Vec<Value>>,
+    #[serde(default)]
+    pub locations: Option<Vec<GraphQLErrorLocation>>,
+}
+
+#[derive(Deserialize)]
+pub struct GraphQLErrorLocation {
+    pub line: i32,
+    pub column: i32,
+}
