1ee4f3ae60fad5c37469686e3ac3a20f09e943e4
0xdmtry <0xdmtry@gmail.com>
2025-09-25 12:52:03 -0400

    backend: init models for the operators snapshot response

--- be-eigen-graph/src/models/operators_snapshot.rs
+++ be-eigen-graph/src/models/operators_snapshot.rs
@@ -1,4 +1,6 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+
+/* --- OperatorsSnapshot: variables --- */
 
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -26,4 +28,55 @@ pub enum OrderDirection {
     Asc,
     #[serde(rename = "desc")]
     Desc,
+}
+
+/* --- OperatorsSnapshot: response --- */
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct OperatorsSnapshotData {
+    pub operators: Vec<OperatorDto>,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct OperatorDto {
+    pub id: String,
+    pub avs_count: i32,
+    pub strategy_count: i32,
+    pub slashing_count: i32,
+    pub last_update_block_timestamp: String,
+    #[serde(default)]
+    pub slashings: Vec<OperatorSlashingDto>,
+    #[serde(default)]
+    pub strategies: Vec<OperatorStrategyLinkDto>,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct OperatorSlashingDto {
+    pub block_timestamp: String,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct OperatorStrategyLinkDto {
+    pub total_shares: String,
+    pub strategy: StrategyLiteDto,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct StrategyLiteDto {
+    pub id: String,
+    pub exchange_rate: String,
+    pub token: TokenDto,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TokenDto {
+    pub id: String,
+    pub symbol: String,
+    pub decimals: i32,
 }
