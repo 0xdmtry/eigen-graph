ec4b383d9f445f7b555066886b408a4ccbc34623
0xdmtry <0xdmtry@gmail.com>
2025-09-24 20:10:36 -0400

    init api

--- be-eigen-graph/src/main.rs
+++ be-eigen-graph/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, response::IntoResponse, routing::get, serve};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+async fn ping() -> impl IntoResponse {
+    "pong"
+}
+
+fn app() -> Router {
+    Router::new().route("/", get(ping))
+}
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("0.0.0.0:8000")
+        .await
+        .expect("Cannot bind port 8000");
+    let app = app().into_make_service_with_connect_info::<SocketAddr>();
+
+    serve(listener, app).await.expect("Cannot serve");
 }
