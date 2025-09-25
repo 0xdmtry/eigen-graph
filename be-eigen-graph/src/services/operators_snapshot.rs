c1b23f0a1e9c4492f3983556df4852ce0e68eb26
0xdmtry <0xdmtry@gmail.com>
2025-09-25 14:50:56 -0400

    backend: init the operators snapshot service functionality

--- be-eigen-graph/src/services/operators_snapshot.rs
+++ be-eigen-graph/src/services/operators_snapshot.rs
@@ -1,1 +1,31 @@
+use crate::api::subgraph::errors::InfraError;
+use crate::api::subgraph::queries::OPERATORS_SNAPSHOT;
+use crate::models::operators_snapshot::{OperatorsSnapshotData, OperatorsSnapshotVars};
+use crate::models::subgraph::{GraphQLRequest, GraphQLResponse};
+use reqwest::{Client, Url};
 
+pub async fn operators_snapshot(
+    client: Client,
+    endpoint: Url,
+    vars: OperatorsSnapshotVars,
+) -> Result<OperatorsSnapshotData, InfraError> {
+    let body = GraphQLRequest {
+        query: OPERATORS_SNAPSHOT,
+        variables: Some(vars),
+    };
+
+    let resp = client
+        .post(endpoint.clone())
+        .json(&body)
+        .send()
+        .await?
+        .error_for_status()?
+        .json::<GraphQLResponse<OperatorsSnapshotData>>()
+        .await?;
+
+    if let Some(errs) = resp.errors {
+        return Err(InfraError::GraphQL(errs));
+    }
+    let data = resp.data.ok_or(InfraError::EmptyData)?;
+    Ok(data)
+}
