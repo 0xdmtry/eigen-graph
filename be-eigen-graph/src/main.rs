8f54f0a8513ba61535832b9032c83f2522a80f2f
0xdmtry <0xdmtry@gmail.com>
2025-09-28 17:53:04 -0400

    infra: open port for the websocket

--- be-eigen-graph/src/main.rs
+++ be-eigen-graph/src/main.rs
@@ -1,6 +1,9 @@
 use axum::serve;
 use be_eigen_graph::app::app;
+use be_eigen_graph::stream::runner::{run_poller, run_ws_server};
+use be_eigen_graph::stream::state::StreamState;
 use std::net::SocketAddr;
+use std::sync::Arc;
 use tokio::net::TcpListener;
 
 #[tokio::main]
@@ -11,6 +14,28 @@ async fn main() {
     let app = app()
         .await
         .into_make_service_with_connect_info::<SocketAddr>();
+
+    tokio::spawn(async move {
+        let stream_state = Arc::new(StreamState::new());
+
+        let ws_srv = tokio::spawn({
+            let ss = stream_state.clone();
+            async move {
+                if let Err(e) = run_ws_server("0.0.0.0:8010", ss).await {
+                    eprintln!("[stream] ws server error: {e:?}");
+                }
+            }
+        });
+
+        let poller = tokio::spawn({
+            let ss = stream_state.clone();
+            async move {
+                run_poller(ss).await;
+            }
+        });
+
+        let _ = tokio::join!(ws_srv, poller);
+    });
 
     serve(listener, app).await.expect("Cannot serve");
 }
